4423704ac548541b33077d5daf9b2c9e8cdcbdaf
Kaledaii <mishant480@gmail.com>
2025-09-20 13:56:28 +0545

    Add files via upload

---
+++ table.py
@@ -0,0 +1,12 @@
+#WAP a function tO print multiplication table of number
+
+n=int(input('enter a number '))
+def table(n):
+    for i in range(1,11):
+        print(n,'*',i,'=',n*i)
+table(n)
+
+
+
+    
+
